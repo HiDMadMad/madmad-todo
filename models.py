41e6e78dce4d7f654f29ca73fc6328e4551835a7
MadMad <151575623+HiDMadMad@users.noreply.github.com>
2025-09-13 20:41:56 +0330

    models.py classes are improved
    added COMPLETED and NOT_COMPLETED statuses to the Status enum for task_status and, add and rewrote two functions for it
    
    rewrote change_name, change_description, and change_importance methods to return detailed operation statuses
    
    improved __str__ methods for better information display
    
    cleaned up the code and added some comments

--- models.py
+++ models.py
@@ -1,44 +1,68 @@
-from messages import MESSAGES
 from enum import Enum
-
 class Status(Enum):
-    SUCCESS = 1
-    NOT_FOUND = -1
+    NOT_COMPLETED = 0  # False
+    COMPLETED = 1  # True
+    SUCCESS = 2
+    # all numbers lower than zero are error codes
+    ERROR = -1
     DUPLICATE = -2
-    EMPTY = 0
+    EMPTY = -3
+    NOT_FOUND = -404
 
 
 class Task():
     task_id_counter = 0
+
+    # importance must be between 1 and 10
     min_importance = 1
     max_importance = 10
 
     def __init__(self, name:str, description:str, importance:int):
-        self.task_status = False
+        self.task_status = Status.NOT_COMPLETED
         self.name = name
         self.description = description
-        self.importance = importance  # control in app.py or ui.py (int, 1<imp<=10)
+        self.importance = importance  # must be checked in app.py or ui.py (int, 1<imp<=10)
         Task.task_id_counter += 1
         self.id = Task.task_id_counter
         
     def __str__(self):
-        return f"{self.id}.{self.name}({self.importance}/{Task.max_importance})\t: {self.description}\n"
+        return f"{self.id}. {self.name} ({self.importance}/{Task.max_importance}) [{self.task_status.name}] : {self.description}\n"
 
     def change_status(self):
-        self.task_status = not self.task_status
-        # if(self.task_status):
-        #     self.task_status = False
-        # else:
-        #     self.task_status = True
+        if(self.task_status == Status.NOT_COMPLETED):
+            self.task_status = Status.COMPLETED
+        else:
+            self.task_status = Status.NOT_COMPLETED
+        return self.task_status
 
-    def change_name(self, new_name):  # control in app.py or ui.py (str)
-        self.name = new_name
+    def is_complete(self):
+        return Status.COMPLETED if self.task_status==Status.COMPLETED else Status.NOT_COMPLETED
+    
+    def change_name(self, new_name:str):  # must be checked in app.py or ui.py (str)
+        if(self.name != new_name):
+            self.name = new_name
+            return Status.SUCCESS
+        elif(self.name == new_name):
+            return Status.DUPLICATE
+        return Status.ERROR
 
-    def change_description(self, new_description):  # control in app.py or ui.py (str)
-        self.description = new_description
+    def change_description(self, new_description:str):  # must be checked in app.py or ui.py (str)
+        if(self.description != new_description):
+            self.description = new_description
+            return Status.SUCCESS
+        elif(self.description == new_description):
+            return Status.DUPLICATE
+        return Status.ERROR
 
-    def change_importance(self, new_importance):  # control in app.py or ui.py (int, 1<imp<=10)
-        self.importance = new_importance
+    def change_importance(self, new_importance:int):
+        if(not (Task.min_importance <=new_importance<= Task.max_importance)):  # new<1 && new>10 --> ERROR
+            return Status.ERROR
+        if(self.importance != new_importance):
+            self.importance = new_importance
+            return Status.SUCCESS
+        elif(self.importance == new_importance):
+            return Status.DUPLICATE
+        return Status.ERROR
 
 
 class TaskList():
@@ -57,7 +81,7 @@ class TaskList():
     
     def find_by_name_in_tl(self, t_name:str):  # tl : task-list
         if(len(self.list_of_tasks)<=0):
-            return Status.EMPTY #  print("list was empty. go and add some tasks to it!")  IN UI.PY 
+            return Status.EMPTY  # "list was empty. go and add some tasks to it!" IN ui.py or app.py
         # for i in range(len(self.list_of_tasks)):
         #     if(self.list_of_tasks[i].name == t_name):
         #         return i
@@ -65,37 +89,36 @@ class TaskList():
             if(task.name == t_name):
                 return index
         else:
-            return Status.NOT_FOUND #  print(f"task \"{t_name}\" was not found")  #  OR print(f' "{variable}"  ')  IN UI.PY
+            return Status.NOT_FOUND  # f"task \"{t_name}\" was not found.") OR f' "{variable}" .') IN ui.py or app.py
 
     def find_by_id_in_tl(self, t_id:any):
         if(len(self.list_of_tasks)<=0):
-            return Status.EMPTY  #  print("list was empty. go and add some tasks to it!")  IN UI.PY
-        # for i in range(len(self.list_of_tasks)):
-        #     if(self.list_of_tasks[i].id == int(t_id)):
-        #         return i
+            return Status.EMPTY
         for index, task in enumerate(self.list_of_tasks):
             if(task.id == t_id):
                 return index
         else:
-            return Status.NOT_FOUND #  print(f"task with ID:{t_id} was not found")  IN UI.PY
+            return Status.NOT_FOUND
+        # if a non-integer is entered instead of the ID, it raises an error
+        # it's will be checked in ui.py or app.py
 
     def add_task(self, task:Task):
         result = self.find_by_name_in_tl(task.name)
-        if(not isinstance(result, Status)):  # type(result)!=Status
-            return Status.DUPLICATE #  print(f"task with name \"{task.name}\" already exists!\n")
+        if(not isinstance(result, Status)):  # type(result)!=Status --> result==index_of_a_task
+            return Status.DUPLICATE  # f"task with name \"{task.name}\" already exists!\n")
         self.list_of_tasks.append(task)
         return Status.SUCCESS
     
     def delete_task_by_id(self, t_id:any):
         result = self.find_by_id_in_tl(t_id) 
-        if(not isinstance(result, Status)):  # type(result)!=Status
-            del self.list_of_tasks[result]
+        if(not isinstance(result, Status)):
+            del self.list_of_tasks[result]  # result is index --> so function is found it --> so it deletes it
             return Status.SUCCESS
         return Status.NOT_FOUND
     
     def delete_task_by_name(self, t_name:str):
         result = self.find_by_name_in_tl(t_name) 
-        if(not isinstance(result, Status)):  # type(result)!=Status
+        if(not isinstance(result, Status)):
             del self.list_of_tasks[result]
             return Status.SUCCESS
         return Status.NOT_FOUND
@@ -104,10 +127,18 @@ class TaskList():
 class ToDoManager():
     def __init__(self):
         self.list_of_task_lists = []
-    
+
+    def __str__(self):
+        if(len(self.list_of_task_lists)<=0):
+            return "(empty)\n"
+        result = ""
+        for task_list in self.list_of_task_lists:
+            result+=('   '+str(task_list))
+        return f"over view :\n{result}\n"
+
     def find_task_list(self, tl_name:str):
         if(len(self.list_of_task_lists)<=0):
-            return Status.EMPTY
+            return Status.EMPTY  # "your directory is empty from task list!"
         for index, task_list in enumerate(self.list_of_task_lists):
             if(task_list.name == tl_name):
                 return index
@@ -128,4 +159,4 @@ class ToDoManager():
             return Status.SUCCESS
         return Status.NOT_FOUND
 
-#MadMad_131
+#MadMad_162
