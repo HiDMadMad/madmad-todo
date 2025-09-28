adc65c39136d7177c97621d9798dcf69b833b1e5
MadMad <151575623+HiDMadMad@users.noreply.github.com>
2025-09-28 18:31:19 +0330

    models final changes before MVP?
    type hints are added
    
    delete functions and some other functions are optimized

--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+from typing import List, Dict, Union
 from enum import Enum
 class Status(Enum):
     NOT_COMPLETED = 0  # False
@@ -7,28 +8,27 @@ class Status(Enum):
     # all numbers lower than zero are error codes
     ERROR = -1
     DUPLICATE = -2
-    EMPTY = -3
-    STOP = -4
+    STOP = -3
+    EMPTY = -4
     NOT_FOUND = -404
 
 
-class Task():
+class Task:
     # importance must be between 1 and 10
-    min_importance = 1
-    max_importance = 10
+    min_importance:int = 1
+    max_importance:int = 10
 
-    def __init__(self, name:str, description:str, importance:int):
+    def __init__(self, name:str, description:str, importance:int) -> None:
         # all checked in ui.py
         self.task_status = Status.NOT_COMPLETED
         self.name = name
         self.description = description
         self.importance = importance
         
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.name} ({self.importance}/{Task.max_importance}) [{self.task_status.name}] : \"{self.description}\"\n"
 
-    def change_status(self):
-        #self.task_status = Status.COMPLETED if self.task_status == Status.NOT_COMPLETED else Status.NOT_COMPLETED
+    def change_status(self) -> Status:
         if(self.task_status == Status.NOT_COMPLETED):
             self.task_status = Status.COMPLETED
         elif(self.task_status == Status.COMPLETED):
@@ -37,11 +37,12 @@ class Task():
             return Status.ERROR
         return self.task_status
 
-    def is_complete(self):
-        #return Status.COMPLETED if self.task_status == Status.COMPLETED else Status.NOT_COMPLETED
-        return True if self.task_status == Status.COMPLETED else False
+    def is_complete(self) -> bool:
+        #return Status.COMPLETED if self.task_status==Status.COMPLETED else Status.NOT_COMPLETED
+        #return True if self.task_status==Status.COMPLETED else False
+        return self.task_status==Status.COMPLETED
         
-    def change_task_description(self, new_description:str):
+    def change_task_description(self, new_description:str) -> Status:
         if(len(new_description)<=0):
             return Status.EMPTY
         if(self.description != new_description):
@@ -50,7 +51,7 @@ class Task():
         else:
             return Status.DUPLICATE
 
-    def change_task_importance(self, new_importance:int):  # is_int must be checked in ui.py
+    def change_task_importance(self, new_importance:int) -> Status:  # is_int must be checked in ui.py
         if(not (Task.min_importance <=new_importance<= Task.max_importance)):  # new<1 || new>10 --> ERROR
             return Status.ERROR
         if(self.importance != new_importance):
@@ -59,7 +60,7 @@ class Task():
         else:
             return Status.DUPLICATE
     
-    def task_to_dict(self):
+    def task_to_dict(self) -> Dict[str, Union[str, int]]:
         return {
                 "name": self.name,
                 "description": self.description,
@@ -68,12 +69,12 @@ class Task():
                 }
 
 
-class TaskList():
-    def __init__(self, name:str):
-        self.list_of_tasks = []
+class TaskList:
+    def __init__(self, name:str) -> None:
+        self.list_of_tasks:List[Task] = []
         self.name = name
         
-    def __str__(self):
+    def __str__(self) -> str:
         if(len(self.list_of_tasks)<=0):
             return f"{self.name}: (empty)\n"
         else:
@@ -82,7 +83,7 @@ class TaskList():
                 result+=('\t'+str(task))
             return f"{result}\n"
     
-    def find_task(self, t_name:str):
+    def find_task(self, t_name:str) -> Union[Task, Status]:
         if(len(self.list_of_tasks)<=0):
             return Status.EMPTY
         for task in self.list_of_tasks:
@@ -91,44 +92,40 @@ class TaskList():
         else:
             return Status.NOT_FOUND
 
-    def add_task(self, task:Task):
-        result = self.find_task(task.name)
-        if(isinstance(result, Task)):
+    def add_task(self, task:Task) -> Status:
+        if(isinstance(self.find_task(task.name), Task)):
             return Status.DUPLICATE
         self.list_of_tasks.append(task)
         return Status.SUCCESS
     
-    def delete_task(self, t_name:str):
-        result = self.find_task(t_name) 
-        if(isinstance(result, Task)):
-            self.list_of_tasks.remove(result)
-            return Status.SUCCESS
+    def delete_task(self, t_name:str) -> Status:
+        for i, task in enumerate(self.list_of_tasks):
+            if task.name == t_name:
+                del self.list_of_tasks[i]
+                return Status.SUCCESS
         return Status.NOT_FOUND
 
-    def change_task_name(self, task:Task, new_name:str):
+    def change_task_name(self, task:Task, new_name:str) -> Status:
         if(isinstance(self.find_task(new_name), Task)):
             return Status.DUPLICATE
-        else:
-            if(len(new_name)<=0):
-                return Status.EMPTY
-            if(task.name != new_name):
-                task.name = new_name
-                return Status.SUCCESS
-            else:
-                return Status.DUPLICATE
+        if(len(new_name)<=0):
+            return Status.EMPTY
+        task.name = new_name
+        return Status.SUCCESS
         
-    def tasklist_to_dict(self):
+    def tasklist_to_dict(self) -> Dict[str, Union[str, List[Dict[str, Union[str, int]]]]]:
+        #                                              |--------List of Tasks-------|
         return {
                 "name": self.name,
                 "list_of_tasks": [task.task_to_dict() for task in self.list_of_tasks]
                 }
 
 
-class ToDoManager():
-    def __init__(self):
-        self.list_of_tasklists = []
+class ToDoManager:
+    def __init__(self) -> None:
+        self.list_of_tasklists:List[TaskList] = []
 
-    def __str__(self):
+    def __str__(self) -> str:
         if(len(self.list_of_tasklists)<=0):
             return "\nover view :\n   (empty)\n"
         result = ""
@@ -136,7 +133,7 @@ class ToDoManager():
             result+=(str(tasklist))
         return f"\nover view :\n{result}\n"
 
-    def find_tasklist(self, tl_name:str):
+    def find_tasklist(self, tl_name:str) -> Union[TaskList, Status]:
         if(len(self.list_of_tasklists)<=0):
             return Status.EMPTY
         for tasklist in self.list_of_tasklists:
@@ -145,35 +142,32 @@ class ToDoManager():
         else:
             return Status.NOT_FOUND
 
-    def add_tasklist(self, tasklist:TaskList):
-        result = self.find_tasklist(tasklist.name)
-        if(isinstance(result, TaskList)):
+    def add_tasklist(self, tasklist:TaskList) -> Status:
+        if(isinstance(self.find_tasklist(tasklist.name), TaskList)):
             return Status.DUPLICATE
         self.list_of_tasklists.append(tasklist)
         return Status.SUCCESS
     
-    def delete_tasklist(self, tl_name:str):
-        result = self.find_tasklist(tl_name)
-        if(isinstance(result, TaskList)):
-            self.list_of_tasklists.remove(result)
-            return Status.SUCCESS
+    def delete_tasklist(self, tl_name:str) -> Status:
+        for i, tasklist in enumerate(self.list_of_tasklists):
+            if tasklist.name == tl_name:
+                del self.list_of_tasklists[i]
+                return Status.SUCCESS
         return Status.NOT_FOUND
     
-    def change_tasklist_name(self, tasklist:TaskList, new_name:str):
+    def change_tasklist_name(self, tasklist:TaskList, new_name:str) -> Status:
         if(isinstance(self.find_tasklist(new_name), TaskList)):
             return Status.DUPLICATE
-        else:
-            if(len(new_name)<=0):
-                return Status.EMPTY
-            if(tasklist.name != new_name):
-                tasklist.name = new_name
-                return Status.SUCCESS
-            else:
-                return Status.DUPLICATE
-
-    def todo_manager_to_dict(self):
+        if(len(new_name)<=0):
+            return Status.EMPTY
+        tasklist.name = new_name
+        return Status.SUCCESS
+        
+    def todo_manager_to_dict(self) -> Dict[str, List[Dict[str, Union[str, List[Dict[str, Union[str, int]]]]]]]:
+        #                                       |                         |--------List of Tasks-------| |
+        #                                       |--------------------List of TaskLists-------------------|
         return {
                 "list_of_tasklists": [tl.tasklist_to_dict() for tl in self.list_of_tasklists]
                }
 
-#MadMad_179
+#MadMad_173
